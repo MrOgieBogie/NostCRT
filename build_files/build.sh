211547f04b7c44661139d135d10038297bf52470
MrOgieBogie <paulthealien921@gmail.com>
2025-07-27 15:16:40 +1000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -16,9 +16,6 @@ fi
 ### Install packages
 set -oue pipefail
 
-echo "Installing additional packages..."
-rpm-ostree install gamemode vulkan-tools htop nvtop power-profiles-daemon
-
 echo "Copying custom scripts..."
 install -D -m 0755 /ctx/firstboot/setup-intel-graphics.sh /usr/local/bin/setup-intel-graphics.sh
 install -D -m 0755 /ctx/firstboot/setup-user.sh /usr/local/bin/setup-user.sh
